2e46ceaddbb51f6a46d71d27202f00bcf2645fa9
pavi <paviguna116@gmail.com>
2024-10-18 22:52:58 +0530

    Update home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ChatBoxComponent } from '../components/chat-box/chat-box.component';
+
+import { addIcons } from 'ionicons';
+import { add } from 'ionicons/icons'
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,53 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // position: string = '';
+  activeSection: string = '';
+  isMobile: boolean = window.innerWidth <= 768;
+  isRightSectionVisible: boolean = false;
 
-  constructor() {}
+  onSectionSelected(section: string) {
+    this.activeSection = section; 
+    if (this.isMobile) {
+      this.isRightSectionVisible = true; 
+    }
+  }
 
+  onBackButtonClicked() {
+    this.isRightSectionVisible = false; 
+  }
+
+  constructor(private modalController: ModalController) {
+    window.addEventListener('resize', (event) => {
+      const target = event?.target as Window;
+      this.isMobile = target.innerWidth <= 768;
+      
+      
+      if (!this.isMobile) {
+        this.isRightSectionVisible = true; 
+      } else {
+        this.isRightSectionVisible = false; 
+      }
+    });
+  }
+
+  ngOnInit() {
+  
+  }
+  
+
+  async openChatModal() {
+    const modal = await this.modalController.create({
+      component: ChatBoxComponent
+    });
+
+    modal.onDidDismiss().then((result) => {
+      if (result.data) {
+        console.log('Selected Chat from Modal:', result.data);
+        
+      }
+    });
+
+    return await modal.present();
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  stripe: {
+    publishableKey: 'pk_test_51OSGOzSIkNIxCI012A74UTB3ldXyLyaCce7UyeE3ekjLmxnwDBMUvb8K4iUrQZhrQNSrA7v8Zjs2hjQZkMevm80L00e9tAfsL4',
+    secretKey: 'sk_test_51OSGOzSIkNIxCI01wwDbnshaiCV5YiHYu52gnWOiCrvsn3mKD5WXVjWEHdnXwGHbbqRHjfoRiug5P7zDZ1ekC49C00vIuc7WRZ'
+  },
+  api: 'http://localhost:3000/'
 };
 
 /*
