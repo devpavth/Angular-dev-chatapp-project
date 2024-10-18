b75f4f8b4af42aa1e23a0b7b7d8f8abf135e8197
pavi <paviguna116@gmail.com>
2024-10-18 22:54:18 +0530

    updated app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// import { LeftSectionComponent } from './components/left-section/left-section.component';
+// import { RightSectionComponent } from './components/right-section/right-section.component';
+
+// import { UserSectionComponent } from './components/user-section/user-section.component';
+// import { DestinationSectionComponent } from './components/destination-section/destination-section.component';
+// import { ExperienceSectionComponent } from './components/experience-section/experience-section.component';
+// import { EventSectionComponent } from './components/event-section/event-section.component';
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent,],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, RouterModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
